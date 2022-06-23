b3fc6a7644e511f48318711d37dd8ce186463f21
Tim Diggins <tim@red56.uk>
2022-06-23 12:06:26 +0100

    update things_spec so it works

--- spec/requests/things_spec.rb
+++ spec/requests/things_spec.rb
@@ -13,12 +13,12 @@ require 'rails_helper'
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/things", type: :request do
-  
+
   # This should return the minimal set of attributes required to create a valid
   # Thing. As you add validations to Thing, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {name: "Some name", model_number: "Some model number"}
   }
 
   let(:invalid_attributes) {
@@ -87,14 +87,14 @@ RSpec.describe "/things", type: :request do
   describe "PATCH /update" do
     context "with valid parameters" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { name: "shiny new name" }
       }
 
       it "updates the requested thing" do
         thing = Thing.create! valid_attributes
         patch thing_url(thing), params: { thing: new_attributes }
         thing.reload
-        skip("Add assertions for updated state")
+        expect(thing.name).to eq("shiny new name")
       end
 
       it "redirects to the thing" do
